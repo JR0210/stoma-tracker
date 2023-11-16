15048eb6cc82770ab2915b3c08d41cc42e4d5cf4
JR0210 <robinson.jacob.e@gmail.com>
2023-11-16 23:06:52 +0000

    feat: Added init CSS testing themes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
+    <main className="flex min-h-screen flex-col items-center justify-between gap-2 lg:p-12">
+      <div className="flex flex-col gap-4 items-center">
         <h1 className="text-3xl lg:text-6xl font-bold text-center">
-          Stoma<span className="">Mate</span>
+          Stoma<span className="text-primary">Mate</span>
         </h1>
-        <h2 className="text-xl lg:text-3xl text-center">
+        <h2 className="text-lg lg:text-xl text-center italic opacity-80">
           Keep tabs on your stoma output
         </h2>
       </div>

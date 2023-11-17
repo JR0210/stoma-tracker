1734d4900090f846a9d896219f99f5de6bd17e7d
JR0210 <robinson.jacob.e@gmail.com>
2023-11-17 00:04:53 +0000

    feat: Changed name to OstoMate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between gap-2 lg:p-12">
       <div className="flex flex-col gap-4 items-center">
         <h1 className="text-3xl lg:text-6xl font-bold text-center">
-          Stoma<span className="text-primary">Mate</span>
+          Osto<span className="text-primary">Mate</span>
         </h1>
         <h2 className="text-lg lg:text-xl text-center italic opacity-80">
           Keep tabs on your stoma output

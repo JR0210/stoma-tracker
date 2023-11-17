39718f9f583c3271f2e0b4d73a1971b9d37d1cb1
JR0210 <robinson.jacob.e@gmail.com>
2023-11-17 00:02:30 +0000

    feat: Added init entry button

---
+++ src/components/EntryButton.tsx
@@ -0,0 +1,9 @@
+const EntryButton = () => {
+  return (
+    <button className="bg-primary transition-colors hover:bg-sky-400 focus:bg-sky-400 text-neutral font-bold py-2 px-4 rounded">
+      Log Entry
+    </button>
+  );
+};
+
+export default EntryButton;

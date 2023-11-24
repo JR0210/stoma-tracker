388455b894c2f2fee967cf87988bbdbb67d423ea
JR0210 <robinson.jacob.e@gmail.com>
2023-11-24 19:26:46 +0000

    feat: Render modal in entry button

--- src/components/EntryButton.tsx
+++ src/components/EntryButton.tsx
@@ -1,8 +1,13 @@
+import Modal from "./Modal";
+
 const EntryButton = () => {
   return (
-    <button className="bg-primary transition-colors hover:bg-sky-400 focus:bg-sky-400 font-bold py-2 px-6 rounded">
-      Log Entry
-    </button>
+    <>
+      <button className="bg-primary transition-colors hover:bg-sky-400 focus:bg-sky-400 font-bold py-2 px-6 rounded">
+        Log Entry
+      </button>
+      <Modal open={true} />
+    </>
   );
 };
 
